15536e15f354615b95987831914c9561eeee541c
Charlie <cmdouglas@gmail.com>
2015-01-20 13:30:23 -0600

    flake

--- toy_roguelike.py
+++ toy_roguelike.py
@@ -1,18 +1,18 @@
 import logging
 import locale
-locale.setlocale(locale.LC_ALL,"")
+locale.setlocale(locale.LC_ALL, "")
 
 from rl import game
-from rl import config
+# from rl import config
 
 
 def main():
     logging.basicConfig(filename='toy_roguelike.log', level=logging.DEBUG)
-    
+
     logging.info('Game Start')
     g = game.Game()
     g.play()
     logging.info('Game End')
-    
+
 if __name__ == '__main__':
     main()
