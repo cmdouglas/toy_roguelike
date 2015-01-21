116947ff80ceeee782e27d1d08ce5cb2cb952943
Charlie <cmdouglas@gmail.com>
2015-01-21 16:28:56 -0600

    linting

--- rl/ui/hud.py
+++ rl/ui/hud.py
@@ -3,6 +3,7 @@ from collections import Counter
 from rl.ui import colors
 from rl.util.tools import clamp
 
+
 class HUD(object):
 
     def objects_of_interest(self, board):
@@ -15,53 +16,48 @@ class HUD(object):
 
         # show things with the hightest interest level first
         interesting_objects.sort(key=lambda o: -1*o.interest_level)
-        
+
         interesting_objects_condensed = []
         c = Counter()
-        
+
         for ob in interesting_objects:
             if ob.__class__ not in c:
                 interesting_objects_condensed.append(ob)
-            
+
             c.update([ob.__class__])
-            
+
         r = []
         for ob in interesting_objects_condensed:
             count = c[ob.__class__]
             description = "A " + ob.describe()
             if count > 1:
                 description = "%s %s" % (count, ob.name_plural)
-                
+
             glyph = ob.glyph
             color = ob.color
-            
+
             r.append({
                 'description': description,
                 'glyphs': min(count, 5)*glyph,
                 'color': color
             })
-            
+
         return r
-        
+
     def status_bar(self, value, max_value, statuscolors=None, bar_length=25):
         value = clamp(value, max_value)
-        
+
         if not statuscolors:
             statuscolors = {
                 'full': colors.light_green,
                 'empty': colors.red
             }
-                
+
         full = int(round((float(value)/max_value) * bar_length))
         empty = bar_length - full
-        
+
         return {
             'full': full,
             'empty': empty,
             'colors': statuscolors
         }
-        
-        
-    
-        
-                
