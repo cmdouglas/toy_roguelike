7f65f2a2b5cf898d3206c11d381c8e136de3fb0a
Charlie <cmdouglas@gmail.com>
2015-06-02 15:23:40 -0500

    start of layout refactor, move objects of interest into their own pane

--- rl/ui/terminal/modes/mapview.py
+++
@@ -1,7 +0,0 @@
-from rl.ui.terminal.modes import Mode
-
-class ExamineMode(Mode):
-    pass
-
-class TargetMode(Mode):
-    pass
---
+++ rl/ui/terminal/modes/world/layout.py
@@ -0,0 +1,46 @@
+import logging
+
+from termapp.layout import Layout
+from termapp.term import term
+
+from rl.ui.terminal.display.panes import board
+from rl.ui.terminal.display.panes import console
+from rl.ui.terminal.display.panes import hud
+
+logger = logging.getLogger('rl')
+
+
+class GameModeLayout(Layout):
+    def __init__(self, world, console):
+        super(GameModeLayout, self).__init__()
+        self.world = world
+        self.console = console
+        self.refresh()
+
+    def refresh(self):
+        console_width = term.width
+        console_height = 4
+
+        hud_width = 44
+        hud_height = (term.height - console_height) - 1
+
+        viewport_width = min((term.width - (hud_width + 2), 80))
+        viewport_height = min(((term.height - console_height) - 1, 80))
+
+        viewport_pos = (0, 0)
+        hud_pos = (viewport_width + 2, 0)
+        console_pos = (0, viewport_height)
+
+        self.board_pane = board.BoardPane(viewport_width, viewport_height, self.world)
+        self.hud_pane = hud.HUDPane(hud_width, hud_height, self.world)
+        self.console_pane = console.ConsolePane(console_width, console_height, self.console)
+
+        self.panes = {
+            viewport_pos: self.board_pane,
+            hud_pos: self.hud_pane,
+            console_pos: self.console_pane
+        }
+
+    def render(self):
+        self.board_pane.center = self.world.player.tile.pos
+        return super().render()
